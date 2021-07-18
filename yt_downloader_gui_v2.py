2f81766fef67a1ef55ae46622795a73c8a680cda
morpheus158 <morpheus158@centrum.cz>
2021-07-18 10:02:21 +0200

    changes

--- yt_downloader_gui_v2.py
+++ yt_downloader_gui_v2.py
@@ -13,7 +13,7 @@ def download_action():
     my_stream = my_video.getbest()
     my_path_value = str(path_value.get())
 
-    if my_path_value.strip() == "":
+    if my_path_value.strip() == "Plocha":
         my_path_value = os.path.join(users_folder_name,os.getlogin(),desktop_folder_name)
 
     if check_int.get() == 1:
@@ -22,13 +22,16 @@ def download_action():
     elif check2_int.get() == 1:
         counter = 0
         title = ""
-        for titles in my_video.allstreams:
-            if titles.mediatype == "audio":
-                title = titles.mediatype + ":" + titles.extension + "@" + titles.bitrate
-            else:
-                title = titles.mediatype + ":" + titles.extension + "@" + titles.resolution
+        audio_bool = False
+        for titles in my_video.streams:
+            #if titles.mediatype == "audio":
+            #    title = titles.mediatype + ":" + titles.extension + "@" + titles.bitrate
+            #else:
+            #    title = titles.mediatype + ":" + titles.extension + "@" + titles.resolution
+            #if titles.mediatype == "normal":
+            title = titles.mediatype + ":" + titles.extension + "@" + titles.resolution
             if title == selected_row:
-                my_stream = my_video.allstreams[counter]
+                my_stream = my_video.streams[counter]
             counter = counter + 1
     else:
         my_stream = my_video.getbest()
@@ -36,19 +39,21 @@ def download_action():
     my_stream.download(my_path_value, quiet=False)
 
     if audio_bool == True:
-        sound_input = AudioSegment.from_file(my_path_value + "\\" + str(my_stream.filename))
+        file = my_path_value + "\\" + str(my_stream.filename)
+        sound_input = AudioSegment.from_file(file)
         if my_stream.filename.endswith("webm"):
             filename = my_stream.filename.replace("webm", "mp3")
         elif my_stream.filename.endswith("m4a"):
             filename = my_stream.filename.replace("m4a", "mp3")
         sound_input.export(my_path_value + "\\" + str(filename))
+        os.remove(file)
 
 def quality_list():
     my_video = pafy.new(url_value.get())
     if check2_int.get() == 1:
         audio_checkbox.grid_forget()
         listall.delete(0,END)
-        for stream in my_video.allstreams:
+        for stream in my_video.streams:
             listall.insert(END,stream)
     if check2_int.get() == 0:
         audio_checkbox.grid(row=4, column=0, sticky="W")
@@ -59,6 +64,9 @@ def get_row(event):
     row_index = listall.curselection()[0]
     selected_row = listall.get(row_index)
 
+def deleteOnClick(*args):
+    path_entry.delete(0, 'end')
+
 window = Tk()
 
 url_value = StringVar()
@@ -68,6 +76,8 @@ url_emtry.grid(row=1, column=1)
 path_value = StringVar()
 path_entry = Entry(window, textvariable=path_value, width=30)
 path_entry.grid(row=2, column=1)
+path_entry.insert(0, 'Plocha')
+path_entry.bind("<Button-1>", deleteOnClick)
 
 title_label = Label(window, text="Pájova appka na stahování z YT")
 title_label.grid(row=0, column=0, columnspan=2)
@@ -78,7 +88,7 @@ url_label.grid(row=1, column=0)
 path_label = Label(window, text="Uložit do: ")
 path_label.grid(row=2, column=0)
 
-down_button = Button(window, text="Stáhnout", command=download_action)
+down_button = Button(window, text="Stáhnout video", command=download_action)
 down_button.grid(row=4, column=0, columnspan=2)
 
 check_int = IntVar()
@@ -86,7 +96,7 @@ audio_checkbox = Checkbutton(window, text="Jen audio (MP3)", variable=check_int)
 audio_checkbox.grid(row=4, column=0, sticky="W")
 
 check2_int = IntVar()
-quality_checkbox = Checkbutton(window, text="Určit kvalitu", variable=check2_int, command=quality_list)
+quality_checkbox = Checkbutton(window, text="Určit kvalitu videa", variable=check2_int, command=quality_list)
 quality_checkbox.grid(row=4, column=1, sticky="E")
 
 listall = Listbox(window, width=60, height=5)
